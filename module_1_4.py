8fd4e6183ce0a3a24477bcc8c0d3c002006ded90
VAKovalenko <83815287+VAKovalenko@users.noreply.github.com>
2024-11-21 01:19:08 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+count_of_solved_dz = 12
+count_of_spend_hours = 1.5
+name_of_course = 'Python'
+time_per_task = count_of_spend_hours / count_of_solved_dz
+print (name_of_course, ', всего задач:', count_of_solved_dz,
+       ', затрачено часов:', count_of_spend_hours,
+       ', среднее время выполнения:', time_per_task, 'часа.')
